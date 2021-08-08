6525373ea0185cefa5c8775df37b47585358be24
jsegobia0 <jsegobia0@yahoo.com>
2021-08-07 22:18:59 -0700

    displays response.name in console

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
+// search button
+var searchBtn = $("<button>");
+searchBtn.on("click", function() {
+  
+})
+
 // Open Weather API
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=" + APIKey;
-var APIKey = "10b1fa858990d6078fd65f88c814eabd";
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Colton&appid=10b1fa858990d6078fd65f88c814eabd";
+// var cityName = $("<input>");
+
 
 // Function to call API
 $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response) {
-    var cityName = $("#search-value");
-    console.log(cityName);
+
+    // Display Colton, CA
+    console.log(response.name);
 
 
 
