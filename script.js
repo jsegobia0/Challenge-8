c0538fdce2fd1e4b79f3b7883fce710f8233803d
jsegobia0 <jsegobia0@yahoo.com>
2021-08-07 21:40:32 -0700

    added jquery script to html

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 // Open Weather API
-var queryURL = "";
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=" + APIKey;
+var APIKey = "10b1fa858990d6078fd65f88c814eabd";
 
 // Function to call API
 $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response) {
+    var cityName = $("#search-value");
+    console.log(cityName);
 
 
 
     
-  }
+  });
